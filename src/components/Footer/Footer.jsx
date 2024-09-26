d1d6ac0764a8766458acc5d9c6fc5aef21d13901
arjun-saseendran <arjunsaseendran@icloud.com>
2024-09-26 18:04:28 +0530

    bux fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='text-white p-4' style={{ background: "#102C57", position: 'absolute', width:'100%'}}>
-      Copyright &copy; 2024 arjunsaseendrn.
+      Copyright &copy; 2024 arjunsaseendran.
     </div>
   );
 }
