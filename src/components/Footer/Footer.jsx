3ac1385500ea29e21057a550a7d62a0acdc90103
arjun-saseendran <arjunsaseendran@icloud.com>
2024-09-26 18:00:43 +0530

    add axio, add state, products

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='text-white p-4' style={{ background: "#102C57", position: 'absolute', bottom:0, width:'100%'}}>
+    <div className='text-white p-4' style={{ background: "#102C57", position: 'absolute', width:'100%'}}>
       Copyright &copy; 2024 arjunsaseendrn.
     </div>
   );
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,26 +3,51 @@ import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function ProductCard() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <Container className="p-5">
       <Row>
-        <Col xs={12} sm={6} md={4} xl={3}>
-          <Card style={{ background: "#D2E0FB" }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button variant="dark" style={{ background: "#102C57" }}>
-                Add to cart
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
+        {products.map((product) => (
+          <Col xs={12} sm={6} md={4} xl={3} key={product.key} className="mt-4">
+            <Card style={{ background: "#D2E0FB" }}>
+              <Card.Img
+                style={{
+                  height: "200px",
+                  objectFit: "contain",
+                  padding: "10px",
+                }}
+                variant="top"
+                src={product.image}
+              />
+              <Card.Body>
+                <Card.Title style={{ maxHeight: "20px", overflow: "hidden" }}>
+                  {product.title}
+                </Card.Title>
+                <Card.Text style={{ maxHeight: "78px", overflow: "hidden" }}>
+                  {product.description}
+                </Card.Text>
+                <Card.Text>${product.price}</Card.Text>
+                <Button variant="dark" style={{ background: "#102C57" }}>
+                  Add to cart
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
